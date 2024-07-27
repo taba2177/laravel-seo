1ddedac9a7f9b040f20b955700f3a19bf3809055
taba2177 <tabadev217@gmail.com>
2024-07-27 10:17:16 +0300

    d

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,5 @@
 
 @section('content')
     <h1>Welcome to the Home Page</h1>
-    <p>This is the home page of the  lalalalallalalalalalalaalal best SEO Laravel sample application.</p>
+    <p>This is the home page of the  lalalalallallalalaalal best SEO Laravel sample application.</p>
 @endsection
